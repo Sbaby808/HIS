80724db9af883e7dff2c5a3cbeccb3486ab03805
Sbaby <corwin808@gmail.com>
2019-07-27 15:10:57 +0800

    配置跨域访问

---
+++ HIS/src/main/java/com/his/utils/CORSConfiguration.java
@@ -0,0 +1,38 @@
+/**
+ * 
+ */
+package com.his.utils;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+/**  
+* @ClassName: CORSConfiguration  
+* @Description: 配置跨域访问
+* @author Sbaby
+* @date 2019年7月27日  下午3:10:20
+*    
+*/
+@Configuration
+public class CORSConfiguration {
+
+	private CorsConfiguration buildConfig() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.addAllowedOrigin("*");
+        corsConfiguration.addAllowedHeader("*");
+        corsConfiguration.addAllowedMethod("*");
+        corsConfiguration.setAllowCredentials(true);
+        return corsConfiguration;
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", buildConfig());
+        return new CorsFilter(source);
+    }
+	
+}
