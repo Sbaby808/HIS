327b92687f883207c4a63a096b2384595f0f537d
cq_test <dell@DESKTOP-JCFFL8H>
2019-07-29 15:38:51 +0800

    111

--- HIS/src/main/java/com/his/service/HosOutService.java
+++ HIS/src/main/java/com/his/service/HosOutService.java
@@ -1,13 +1,16 @@
 package com.his.service;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.his.dao.IHosOutDao;
+import com.his.pojo.OutHospitaiRecord;
 
 
 @Service
@@ -17,7 +20,12 @@ public class HosOutService {
 	@Autowired
 	private IHosOutDao hosOutDao;
 	
-	/*public Map getAllOutRecords(int curpage,int pagesize){
-		
-	}*/
+	public Map getAllOutRecords(int curpage,int pagesize){
+		List <OutHospitaiRecord> list = hosOutDao.getAllOutRecords(PageRequest.of(curpage-1, pagesize));
+		long total = hosOutDao.count();
+		Map map = new HashMap<>();
+		map.put("list", list);
+		map.put("total", total);
+		return map;
+	}
 }
--- HIS/src/test/java/com/his/test2.java
+++ HIS/src/test/java/com/his/test2.java
@@ -1,7 +1,10 @@
 package com.his;
 
+import java.util.Date;
+
 public class test2 {
 	public static void main(String[] args) {
-		System.out.println("test2");
+		//System.out.println("test2");
+		System.out.println(new Date());
 	}
 }
