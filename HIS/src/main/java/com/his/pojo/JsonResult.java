3be7f03afa962750db35e38d5234f20ee5cbbd97
Sbaby <corwin808@gmail.com>
2019-07-30 14:10:37 +0800

    修改JsonResult类

--- HIS/src/main/java/com/his/pojo/JsonResult.java
+++ HIS/src/main/java/com/his/pojo/JsonResult.java
@@ -2,11 +2,28 @@ package com.his.pojo;
 
 /**  
 * @ClassName: JsonResult  
-* @Description: TODO(这里用一句话描述这个类的作用)  
+* @Description: JsonResult返回类
 * @author Sbaby
 * @date 2019年7月30日  下午12:21:19
 *    
 */
 public class JsonResult {
 
+	private String status = null;
+	private Object result = null;
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	public Object getResult() {
+		return result;
+	}
+	public void setResult(Object result) {
+		this.result = result;
+	}
+	
+	
+	
 }
