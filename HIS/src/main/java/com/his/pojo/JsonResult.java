1ccac4ac71f23b6ded296cb05be317dba6f64530
Sbaby <corwin808@gmail.com>
2019-07-30 14:05:38 +0800

    添加JsonResult类

---
+++ HIS/src/main/java/com/his/pojo/JsonResult.java
@@ -0,0 +1,12 @@
+package com.his.pojo;
+
+/**  
+* @ClassName: JsonResult  
+* @Description: TODO(这里用一句话描述这个类的作用)  
+* @author Sbaby
+* @date 2019年7月30日  下午12:21:19
+*    
+*/
+public class JsonResult {
+
+}
