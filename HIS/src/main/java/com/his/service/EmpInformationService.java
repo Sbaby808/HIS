69f255d99b4ab9b5c6b3fdb50ba300eb2e191810
Sbaby <corwin808@gmail.com>
2019-07-31 14:16:09 +0800

    添加测试插入员工方法

--- HIS/src/main/java/com/his/service/EmpInformationService.java
+++ HIS/src/main/java/com/his/service/EmpInformationService.java
@@ -1,7 +1,11 @@
 package com.his.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import com.his.dao.IEmpInformationDao;
+import com.his.pojo.EmpInformation;
 
 /**  
 * @ClassName: EmpInformationService  
@@ -14,5 +18,22 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 @Transactional(rollbackFor=Exception.class)
 public class EmpInformationService {
+	
+	@Autowired
+	private IEmpInformationDao empInformationDao;
+	
+	/**
+	 * 
+	* @Title:addTestEmp
+	* @Description:添加测试员工信息
+	* @param:@param empInformation
+	* @return:void
+	* @throws
+	* @author:Sbaby
+	* @Date:2019年7月31日 下午2:02:29
+	 */
+	public void addTestEmp(EmpInformation empInformation) {
+		empInformationDao.save(empInformation);
+	}
 
 }
--- HIS/src/test/java/com/his/HisApplicationTests.java
+++ HIS/src/test/java/com/his/HisApplicationTests.java
@@ -2,15 +2,42 @@ package com.his;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import com.his.pojo.EmpInformation;
+import com.his.service.EmpInformationService;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class HisApplicationTests {
 
+	@Autowired
+	private EmpInformationService empInformationService;
+	
 	@Test
 	public void contextLoads() {
+	}
+	
+	/**
+	 * 
+	* @Title:addTestEmp
+	* @Description:添加测试员工信息
+	* @param:
+	* @return:void
+	* @throws
+	* @author:Sbaby
+	* @Date:2019年7月31日 下午2:03:37
+	 */
+	@Test
+	public void addTestEmp() {
+		EmpInformation admin = new EmpInformation();
+		admin.setYgxh("ADMINTST");
+		admin.setYgName("王富贵");
+		admin.setYgSex("男");
+		admin.setYgPassword("admin123");
+		empInformationService.addTestEmp(admin);
 	}
 
 }
