5ab3cdea1005beda4c892fcc42852ae369aba9d4
Sbaby <corwin808@gmail.com>
2019-07-31 11:03:29 +0800

    开发版LoginController

---
+++ HIS/src/main/java/com/his/controller/LoginController.java
@@ -0,0 +1,33 @@
+package com.his.controller;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.his.pojo.EmpInformation;
+import com.his.pojo.JsonResult;
+
+/**  
+* @ClassName: LoginController  
+* @Description: 登录控制器
+* @author Sbaby
+* @date 2019年7月30日  下午2:11:06
+*    
+*/
+@Controller
+public class LoginController {
+
+	@PostMapping("/login")
+	@ResponseBody
+	public JsonResult login(@RequestBody EmpInformation empInformation) {
+		JsonResult result = new JsonResult();
+		result.setResult(empInformation);
+		result.setStatus("ok");
+		return result;
+	}
+	
+}
