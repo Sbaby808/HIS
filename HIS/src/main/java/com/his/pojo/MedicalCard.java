db0f7cee8ff25c087ef6e12daa1f9f65b904fe01
Sbaby <corwin808@gmail.com>
2019-08-02 09:35:44 +0800

    修改就诊卡实体类(添加联系电话属性) alter table medical_card add (tel varchar2(20));

--- HIS/src/main/java/com/his/pojo/MedicalCard.java
+++ HIS/src/main/java/com/his/pojo/MedicalCard.java
@@ -49,6 +49,8 @@ public class MedicalCard implements Serializable {
 	private String linkRelation;
 
 	private String married;
+	
+	private String tel;
 
 	private String passwd;
 
@@ -104,6 +106,14 @@ public class MedicalCard implements Serializable {
 
 	public void setAge(BigDecimal age) {
 		this.age = age;
+	}
+
+	public String getTel() {
+		return tel;
+	}
+
+	public void setTel(String tel) {
+		this.tel = tel;
 	}
 
 	public Date getBirthday() {
