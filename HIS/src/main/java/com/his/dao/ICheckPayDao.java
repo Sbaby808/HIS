77432b702eb678d429f32d6152644da31605b57a
trc <trc@LAPTOP-U915JUIP>
2019-08-12 17:33:18 +0800

    检查小项，检查项，手术用药门诊

--- HIS/src/main/java/com/his/dao/ICheckPayDao.java
+++ HIS/src/main/java/com/his/dao/ICheckPayDao.java
@@ -2,6 +2,7 @@ package com.his.dao;
 
 import java.util.List;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
@@ -28,4 +29,8 @@ public interface ICheckPayDao extends CrudRepository<CheckPay, String>{
 	 */
 	@Query("from CheckPay c")
 	public List <CheckPay> getAllCheckPay();
+	@Query(value="from CheckPay c where c.checkPayName like?1")
+	public List<CheckPay> getcheckbysou(String sou,Pageable page);
+	@Query(value="select count(1) from CheckPay c where c.checkPayName like?1")
+	public long getcount(String sou);
 }
