f243776709a40bfa7ee5aad93dcb16fa4a6ced8b
crazy_long <2994468821@qq.com>
2019-08-16 10:23:44 +0800

    采购计划加强

--- HIS/src/main/java/com/his/dao/IPurchaseDetailsDao.java
+++ HIS/src/main/java/com/his/dao/IPurchaseDetailsDao.java
@@ -1,5 +1,9 @@
 package com.his.dao;
 
+import java.util.List;
+
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import com.his.pojo.PurchaseDetail;
@@ -14,4 +18,43 @@ import com.his.pojo.PurchaseDetailPK;
 */
 public interface IPurchaseDetailsDao extends CrudRepository<PurchaseDetail, PurchaseDetailPK>{
 
+	/**
+	* @Title:getForCgid
+	* @Description:根据采购id查找对应的明细
+	* @param:@param cgid
+	* @param:@return
+	* @return:List<PurchaseDetail>
+	* @throws
+	* @author:crazy_long
+	* @Date:2019年8月14日 下午3:30:09
+	 */
+	@Query("from PurchaseDetail p where p.purchase.cgId = ?1")
+	public List<PurchaseDetail> getForCgid(String cgid);
+	
+	/**
+	* @Title:getForCgidByPage
+	* @Description:根据采购id分页查询对应的明细记录
+	* @param:@param cgid
+	* @param:@param page
+	* @param:@return
+	* @return:List<PurchaseDetail>
+	* @throws
+	* @author:crazy_long
+	* @Date:2019年8月15日 下午5:33:41
+	 */
+	@Query("from PurchaseDetail p where p.purchase.cgId = ?1")
+	public List<PurchaseDetail> getForCgidByPage(String cgid,Pageable page);
+	
+	/**
+	* @Title:getForCgidByPageCount
+	* @Description:根据采购id分页查询对应的明细记录的条数
+	* @param:@param cgid
+	* @param:@return
+	* @return:List<PurchaseDetail>
+	* @throws
+	* @author:crazy_long
+	* @Date:2019年8月15日 下午5:35:32
+	 */
+	@Query("select count(*) from PurchaseDetail p where p.purchase.cgId = ?1")
+	public int getForCgidByPageCount(String cgid);
 }
--- HIS/src/main/java/com/his/service/PurchaseDetailsService.java
+++ HIS/src/main/java/com/his/service/PurchaseDetailsService.java
@@ -1,7 +1,20 @@
 package com.his.service;
 
+import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import com.his.dao.IPurchaseDetailsDao;
+import com.his.pojo.Purchase;
+import com.his.pojo.PurchaseDetail;
+import com.his.pojo.PurchaseDetailPK;
+import com.his.utils.ServiceException;
 
 /**  
 * @ClassName: PurchaseDetailsService  
@@ -15,8 +28,123 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional(rollbackFor=Exception.class)
 public class PurchaseDetailsService {
 	
-	public void addPurchaseMX() {
-		
+	@Autowired
+	private IPurchaseDetailsDao purchaseDetailsDao;
+	
+	/**
+	* @Title:addPurchaseAndpurchaseDetail
+	* @Description:往已经存在的采购计划中添加药品
+	* @param:@param purchaseDetail
+	* @param:@throws ServiceException
+	* @return:void
+	* @throws
+	* @author:crazy_long
+	* @Date:2019年8月14日 下午10:10:59
+	 */
+	public void appendToDetail(List<PurchaseDetail> purchaseDetail) throws ServiceException{
+		//获取主表的信息
+		Purchase purchase = purchaseDetail.get(0).getPurchase();
+		//生成该采购计划的主键
+		String cgId = purchase.getCgId();
+		try {
+			//循环插入采购明细
+			for (PurchaseDetail p : purchaseDetail) {
+				//建立联合主键
+				PurchaseDetailPK purchaseDetailPK = new PurchaseDetailPK();
+				purchaseDetailPK.setCgId(cgId);
+				purchaseDetailPK.setYpId(p.getDrugInformation().getYpId());
+				p.setId(purchaseDetailPK);
+				purchaseDetailsDao.save(p);
+			} 
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw new ServiceException("追加采购计划明细失败");
+		}
+	}
+	
+	/**
+	* @Title:getAllPurCheDetailsByCgid
+	* @Description:根据采购id查找对应的明细
+	* @param:@param cgid
+	* @param:@return
+	* @return:List<PurchaseDetail>
+	* @throws
+	* @author:crazy_long
+	* @Date:2019年8月14日 下午3:34:28
+	 */
+	public List<PurchaseDetail> getAllPurCheDetailsByCgid(String cgid){
+		return purchaseDetailsDao.getForCgid(cgid);
+	}
+	
+	/**
+	* @Title:getDetailByCgidForPage
+	* @Description:根据采购id分页查找对应的明细
+	* @param:@param curPage
+	* @param:@param pageSize
+	* @param:@param cgId
+	* @param:@return
+	* @return:Map
+	* @throws
+	* @author:crazy_long
+	* @Date:2019年8月15日 下午5:42:32
+	 */
+	public Map getDetailByCgidForPage(int curPage,int pageSize,String cgId) {
+		Map map = new HashMap();
+		List<PurchaseDetail> list = purchaseDetailsDao.getForCgidByPage(cgId,PageRequest.of(curPage - 1, pageSize));
+		int total = purchaseDetailsDao.getForCgidByPageCount(cgId);
+		map.put("list", list);
+		map.put("total", total);
+		return map;
+	}
+	
+	/**
+	* @Title:delOneDetail
+	* @Description:删除一个采购明细
+	* @param:@param ypId
+	* @param:@param cgId
+	* @param:@throws ServiceException
+	* @return:void
+	* @throws
+	* @author:crazy_long
+	* @Date:2019年8月14日 下午4:58:07
+	 */
+	public void delOneDetail(String ypId,String cgId) throws ServiceException{
+		PurchaseDetailPK purchaseDetailPK = new PurchaseDetailPK();
+		purchaseDetailPK.setCgId(cgId);
+		purchaseDetailPK.setYpId(ypId);
+		try {
+			purchaseDetailsDao.deleteById(purchaseDetailPK);
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw new ServiceException("删除采购明细失败");
+		}
+	}
+	
+	/**
+	* @Title:delOneDetail
+	* @Description:修改某一个采购明细的数量
+	* @param:@param ypId
+	* @param:@param cgId
+	* @param:@throws ServiceException
+	* @return:void
+	* @throws
+	* @author:crazy_long
+	* @Date:2019年8月14日 下午4:58:07
+	 */
+	public void updateOneDetailNumber(String ypId,String cgId,int purchaseNumber) throws ServiceException{
+		PurchaseDetailPK purchaseDetailPK = new PurchaseDetailPK();
+		purchaseDetailPK.setCgId(cgId);
+		purchaseDetailPK.setYpId(ypId);
+		try {
+			PurchaseDetail purchaseDetail = purchaseDetailsDao.findById(purchaseDetailPK).get();
+			if(purchaseDetail!=null) {
+				purchaseDetail.setPurchaseNum(new BigDecimal(purchaseNumber));
+			}
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw new ServiceException("修改采购明细数量失败");
+		}
 	}
 
 }
